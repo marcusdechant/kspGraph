226431888c2c41c8711c70ffd6de1f664a203069
marcusdechant <marcusdechant@gmail.com>
2024-01-13 22:55:00 -0700

    1st Review down to flight_data(). done for today

--- FLIGHTS/data.py
+++ FLIGHTS/data.py
@@ -27,9 +27,9 @@ def test_params():
 
     file_test='HM_CAT'
     test_ver=3
-    test_table1=('ttest{}'.format(test_ver))
+    test_table1=(f'ttest{test_ver}')
     test_table2=('ftest')
-    test_table3=('dtest{}'.format(test_ver))
+    test_table3=(f'dtest{test_ver}')
     test_pad='KSC Pad 1'
     test_schema='test'
 
@@ -62,7 +62,7 @@ def start():
     test=test_params()
     CO()
 
-    print('{} Kerbal Space Program Launch Telemetry Processing and Database Insertion'.format(nl))
+    print(f'{nl} Kerbal Space Program Launch Telemetry Processing and Database Insertion')
     print(' Python/Postgres')
     print(' By: Marcus Dechant')
 
@@ -81,7 +81,7 @@ def user():
         # where class is the family of launch vehicle, or in cases when multiple missions are grouped together as in the case of orbital construction (ex. hailmary, KOSS).
         # where mission refers to either the individual mission name ex. HM_CORE, HM_CST, Cache Alpha. Some refer to the payload while some are sequential.
         # where number is when payload named missions required more than one launch (ex. HM_Eng_4)
-        # file_raw=input('{} Flight Name?                                     : '.format(nl))     # user specific flight name (name of telmu csv data)
+        # file_raw=input(f'{nl} Flight Name?                                     : ')     # user specific flight name (name of telmu csv data)
         fn=file_raw.split("_") # fn[0,1]
         
         if str.lower(fn[0])=='cache':           # if flight is named cache (obselete) rename to Callisto                                                               
@@ -119,7 +119,7 @@ def user():
         payc='n'
         fopt='n'
     else:
-        bstc=input('{} Did the Launch Vehicle have boosters? (y/n)      : '.format(nl))                  # user defines if the launch vehicle had boosters
+        bstc=input(f'{nl} Did the Launch Vehicle have boosters? (y/n)      : ')                  # user defines if the launch vehicle had boosters
         payc=input(' Did the Launch Vehicle need a fairing? (y/n)     : ')                  # user defines if launch vehicle staged a payload
         fopt=input(' Did the Launch Vehicle release a payload? (y/n)  : ')                # user defines if launch vehicle staged a fairing
         
@@ -155,11 +155,11 @@ def source():
     csv=inc[0][0]
     folder=inc[0][3][0]
 
-    print('{} Reading {} Telemetry CSV file. [{}\{}.csv]'.format(nl,csv,folder,csv))
+    print(f'{nl} Reading {csv} Telemetry CSV file. [{folder}\{csv}.csv]')
     
     df=pd.read_csv(r'{}\{}.csv'.format(folder,csv))    # extracts telmetry csv to dataframe
     col=df.columns.values                                      # defines columns as col[0,1,2,3,n]
-    # print('{}{}{}'.format(nl,col,nl))                        # column name print out
+    # print(f'{ml}{col}{nl}')                        # column name print out
     name=['TIME','STG','ASL','DRG','SFVEL','ORVEL',  
           'MASS','ACC','Q','AoA','AoS','AoD',
           'Atru','PITCH','gLS','dLS','sLS','DLTV']             # shortened column names
@@ -167,7 +167,7 @@ def source():
     while a<len(col):                                          # update column names to shorter names
         col[a]=name[a]
         a+=1
-    # print('{}{}{}'.format(nl,col,nl))                        # new column name print out
+    # print(f'{nl}{col}{nl}')                        # new column name print out
 
     return(col,df,inc)
 
@@ -211,7 +211,7 @@ def processing():
             rate.fillna(0,inplace=True)
         c+=1
 
-    print('{} Complete!{}{}'.format(cr[6],cr[0],nl))
+    print(f'{cr[6]} Complete!{cr[0]}{nl}')
 
     return(col,df,inc)
 
@@ -223,11 +223,11 @@ def flight_telemetry():
     cr=inc[3]
     test=inc[4]
 
-    print(' Connecting to PostgreSQL Database: {}[ksp]{}'.format(cr[7],cr[0]))
+    print(f' Connecting to PostgreSQL Database: {cr[7]}[ksp]{cr[0]}')
     conn=psql()
     cur=conn.cursor()
     sl(t[5])
-    print('{} Connected!{}{}'.format(cr[6],cr[0],nl))
+    print(f'{cr[6]} Connected!{cr[0]}{nl}')
     sch=['callisto','hailmary']                         # schema list, based on LV naming conventions the LV class is the database schema
     # if(cl=='Callisto' or cl=='Cache'):          # Callisto or Cache Program Class LVs, similar in size and power, upgraded and refined. Orbital Supply Program.
     #     schema=(sch[0])                               # set correct schema from list
@@ -235,19 +235,18 @@ def flight_telemetry():
     #     schema=(sch[1])                               # set correct schema from list
     # table='telemetry'                                 # flights table differs from the other as its a record of all flights in one program or project
 
-    schema_table=("{}.{}".format(test[5],test[2]))         # set schema name for query
+    schema_table=('.'.join((test[5],test[2])))         # set schema name for query
     
-    print(' Creating Table:{} [ksp.{}]{}'.format(cr[7],schema_table,cr[0]))
+    print(f' Creating Table: {cr[7]} [ksp.{schema_table}]{cr[0]}')
     
     while(True):                                                                            # telemetry table creation
         try:
-            TBL=('''CREATE TABLE {} () INHERITS ({});
-                 '''.format(schema_table,temp[1]))                                         # create table command
+            TBL=(f'''CREATE TABLE {schema_table} () INHERITS ({temp[1]});''')                                         # create table command
             cur.execute(TBL)                                                                # execute sql command
             break                                                                           # break while loop
         except(errors.lookup(errorcodes.DUPLICATE_TABLE)):                                  # if table already exists
             while(True):                                        
-                ovrw=input('{} Table Exists. Overwrite? (y/n):{} '.format(cr[1],cr[0]))  # ask user if they wish to overwrite
+                ovrw=input(f'{cr[1]} Table Exists. Overwrite? (y/n): {cr[0]} ')  # ask user if they wish to overwrite
                 ovry=(ovrw=='y')
                 ovrx=(ovrw=='n')
                 if(ovry)or(ovrx):
@@ -260,20 +259,20 @@ def flight_telemetry():
                         cur.execute(DUP)                                                    # execute sql command
                         break                                                               # break while loop
                     else:
-                        print('{}{} Process Canceled...{}{}'.format(nl,cr[1],cr[0],nl))     # user does not want to overwritte
+                        print(f'{nl}{cr[1]} Process Canceled... {cr[0]}{nl}')     # user does not want to overwritte
                         exit()                                                              # exit program
                 else:
                     print(' Incorrect Input...')                                            # neither yes or no
                     continue                                                                # continue while loop
             continue                                                                        # if table exists, delete and loop            
-    print('{} Complete!{}{}'.format(cr[6],cr[0],nl))
+    print(f'{cr[6]} Complete!{cr[0]}{nl}')
     print(' Populating table... ')
     for (i,r) in df.iterrows():                                     # iterate through rows
         a=tuple(r.values)                                           # turn each row into a tuple
         SQL=(f'''INSERT INTO {schema_table} VALUES %s;''')          # sql command
         cur.execute(SQL, (a,)) 
     sl(t[6])
-    print('{} Complete!{}{}'.format(cr[6],cr[0],nl))
+    print(f'{cr[6]} Complete!{cr[0]}{nl}')
 
     return(col,df,inc,cur)
 
@@ -327,126 +326,33 @@ def flight_info():
     #     schema=(sch[1])                               # set correct schema from list
     # table='flights'                                   # flights table differs from the other as its a record of all flights in one program or project
         
-    schema_table=('{}.{}'.format(test[5],test[6]))         #set schema name for query)
+    schema_table=('.'.join((test[5],test[6])))         #set schema name for query)
     SQL=(f'''INSERT INTO {schema_table} VALUES %s ON CONFLICT DO NOTHING;''')          # sql command
     cur.execute(SQL,(put,))
     
     time=pd.to_timedelta(data[2],'sec')                     # translating raw time (seconds) into clock time for readability
     mm=time.components.minutes                           # split minutes off
-    ss=time.components.seconds                             # spilt seconds off
+    ss=time.components.seconds                           # spilt seconds off
     ms=time.components.milliseconds                    # split milliseconds off
-    time=('{:.2f}:{:.2f}.{:.3f}'.format(mm,ss,ms))                      # reform
+    time=(f'{mm:02d}:{ss:02d}.{ms:03d}')                      # reform
 
-    print('{} Database: [ksp.{}] {}{}'.format(cr[5],schema_table,cr[0],nl))
-    print('{} Flight Information {}{}'.format(cr[2],cr[0],nl))
-    print('{}  - Flight Name:              {} {}'.format(cr[1],inc[0][1],cr[0]))
-    print('  - Launch Complex/Pad:       {}'.format(inc[1][0]))
-    print('{}  - Flight Time:              {} {}'.format(cr[6],time,cr[0]))
-    print('  - Flight Time (Raw):        {:.3f} s'.format(data[2]))
-    print('{}  - Final Altitude:           {:.3f} km{}'.format(cr[7],(data[3]/km),cr[0]))
-    print('  - Downrange:                {:.3f} km'.format(data[4]/km))
-    print('  - Orbital Velocity:         {:.4f} km/s'.format(data[5]/km))    
-    print('{}  - Launch Vehicle Mass:      {:.3f} t{}'.format(cr[3],data[6],cr[0]))
-    print('  - Payload Mass:             {:.3f} t'.format(data[7]))
-    print('  - Launch Vehicle DeltaV:    {:.0f} m/s{}'.format(data[8],nl))
+    print(f' Database: [ksp.{schema_table}]')
+    print(f' Flight Information{nl}')
+    print(f'{cr[1]}  - Flight Name:              {inc[0][1]} {cr[0]}')
+    print(       f'  - Launch Complex/Pad:       {inc[1][0]}')
+    print(f'{cr[6]}  - Flight Time:              {time} {cr[0]}')
+    print(       f'  - Flight Time (Raw):        {data[2]:.3f} s')
+    print(f'{cr[7]}  - Final Altitude:           {data[3]/km:.3f} km {cr[0]}')
+    print(       f'  - Downrange:                {data[4]/km:.3f} km')
+    print(       f'  - Orbital Velocity:         {data[5]/km:.4f} km/s')    
+    print(f'{cr[3]}  - Launch Vehicle Mass:      {data[6]:.3f} t {cr[0]}')
+    print(       f'  - Payload Mass:             {data[7]:.3f} t')
+    print(       f'  - Launch Vehicle DeltaV:    {data[8]:.0f} m/s {nl}')
     
     raw.append(data)
 
     exit()
-          
-    
-
-    flin={'name'    :[ict],
-          'pad'     :[idp],
-          'time'    :[d[0]],
-          'altitude':[d[1]],
-          'range'   :[d[2]],
-          'speed'   :[d[3]],
-          'lv_mass' :[d[4]],
-          'pay_mass':[d[5]],
-          'delta_v' :[d[6]],
-          'bst_q'   :[opts[0]],
-          'pay_q'   :[opts[1]],
-          'fair_q'  :[opts[2]]}             # flight info dataframe
-    
-    fl=pd.DataFrame(flin)
-    fol=fl.columns.values                   #flight info columns
-
-    data=[file,
-          pad,
-          fl[fol[2]][0],
-          fl[fol[3]][0],
-          fl[fol[4]][0],
-          fl[fol[5]][0],
-          fl[fol[6]][0],
-          fl[fol[7]][0],
-          fl[fol[8]][0],
-          opts[0],
-          opts[1],
-          opts[2]]                                      #flight info data
-    
-    # sch=['callisto','hailmary']                       # schema list, based on LV naming conventions the LV class is the database schema
-    # if(cl=='Callisto' or cl=='Cache'):          # Callisto or Cache Program Class LVs, similar in size and power, upgraded and refined. Orbital Supply Program.
-    #     schema=(sch[0])                               # set correct schema from list
-    # if(cl=='HM' or cl=='Hailmary'):             # HM or Hailmary Project Class LVs, differ in size & payload. Orbital Construction Project.
-    #     schema=(sch[1])                               # set correct schema from list
-    schema='test'
-    table='ftest'
-    # table='flights'                                   # flights table differs from the other as its a record of all flights in one program or project
-    schema_table=('{}.{}'.format(schema,table))         #set schema name for query
-
-    SQL=('''INSERT INTO 
-            {} 
-            ({},{},{},{},{},{},{},{},{},{},{},{})
-            VALUES 
-             ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')
-            ON CONFLICT DO NOTHING;
-            '''.format(schema_table,
-            fol[0],fol[1],fol[2],fol[3],fol[4],
-            fol[5],fol[6],fol[7],fol[8],fol[9],
-            fol[10],fol[11],
-            data[0],data[1],data[2],data[3],
-            data[4],data[5],data[6],data[7],
-            data[8],data[9],data[10],data[11]))             # sql insert for postgres
-    cur.execute(SQL)                                        # execute postgres insert
-     
-    time=pd.to_timedelta(data[2],'sec')                     # translating raw time (seconds) into clock time for readability
-    mm=time.components.minutes                              # split minutes off
-    ss=time.components.seconds                              # spilt seconds off
-    ms=time.components.milliseconds                         # split milliseconds off
-    time=('{}:{}.{}'.format(mm,ss,ms))                      # reform
-
-    print('{} Database: [ksp.{}] {}{}'.format(cr[5],schema_table,cr[0],nl))
-    sl(t1) 
-    print('{} Flight Information {}{}'.format(cr[2],cr[0],nl))
-    sl(t1)
-    print('{}  - Flight Name:              {} {}'.format(cr[1],flnm,cr[0]))
-    sl(t2)
-    print('  - Launch Complex/Pad:       {}'.format(data[1]))
-    sl(t2)
-    print('{}  - Flight Time:              {} {}'.format(cr[6],time,cr[0]))
-    sl(t2)
-    print('  - Flight Time (Raw):        {:.3f} s'.format(data[2]))
-    sl(t2)
-    print('{}  - Final Altitude:           {:.3f} km{}'.format(cr[7],(data[3]/km),cr[0]))
-    sl(t2)
-    print('  - Downrange:                {:.3f} km'.format(data[4]/km))
-    sl(t2)
-    print('  - Orbital Velocity:         {:.4f} km/s'.format(data[5]/km))    
-    sl(t2)
-    print('{}  - Launch Vehicle Mass:      {:.3f} t{}'.format(cr[3],data[6],cr[0]))
-    sl(t2)
-    print('  - Payload Mass:             {:.3f} t'.format(data[7]))
-    sl(t2)
-    print('  - Launch Vehicle DeltaV:    {:.0f} m/s{}'.format(data[8],nl))
-    sl(t3)
-    # print('Raw Data: {}{}'.format(data,nl))
-    # print('SQL: {}{}'.format(SQL,nl))
-    # print('DataFrame{}{}{}'.format(nl,fl,nl))
-
-    raw.append(data)
-    sl(t4)
-    
+        
     return(col,df,fol,fl,raw,cur)
 
 def flight_data():
